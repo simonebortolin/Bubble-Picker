a2695aac4ea9ae95a8042746c8e15f9d67f6721e
Anas Ambri <anas.ambri@gmail.com>
2017-08-21 14:05:06 +0200

    Add selectedColor and selectedTextColor for use when objects are selected

--- bubblepicker/src/main/java/com/igalata/bubblepicker/rendering/Item.kt
+++ bubblepicker/src/main/java/com/igalata/bubblepicker/rendering/Item.kt
@@ -77,25 +77,37 @@ data class Item(val pickerItem: PickerItem, val circleBody: CircleBody) {
         if (isSelected) drawImage(canvas)
         drawBackground(canvas, isSelected)
         drawIcon(canvas)
-        drawText(canvas)
+        drawText(canvas, isSelected)
 
         return bitmap
     }
 
-    private fun drawBackground(canvas: Canvas, withImage: Boolean) {
+    private fun drawBackground(canvas: Canvas, isSelected: Boolean) {
         val bgPaint = Paint()
         bgPaint.style = Paint.Style.FILL
-        pickerItem.color?.let { bgPaint.color = pickerItem.color!! }
+        pickerItem.color?.let {
+            if (isSelected) {
+                bgPaint.color = pickerItem.selectedColor!!
+            } else {
+                bgPaint.color = pickerItem.color!!
+            }
+
+        }
         pickerItem.gradient?.let { bgPaint.shader = gradient }
-        if (withImage) bgPaint.alpha = (pickerItem.overlayAlpha * 255).toInt()
+        if (isSelected) bgPaint.alpha = (pickerItem.overlayAlpha * 255).toInt()
         canvas.drawRect(0f, 0f, bitmapSize, bitmapSize, bgPaint)
     }
 
-    private fun drawText(canvas: Canvas) {
+    private fun drawText(canvas: Canvas, isSelected: Boolean = false) {
         if (pickerItem.title == null || pickerItem.textColor == null) return
 
         val paint = TextPaint(Paint.ANTI_ALIAS_FLAG).apply {
-            color = pickerItem.textColor!!
+            if (isSelected) {
+                color = pickerItem.selectedTextColor!!
+            } else {
+                color = pickerItem.textColor!!
+            }
+
             textSize = pickerItem.textSize
             typeface = pickerItem.typeface
         }
